9e901128c4eafeb383aa803e600dcd40f9837255
Neeraj Sharma <sharman123@gmail.com>
2020-10-25 23:17:05 +0100

    initial push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyformComponent } from './myform/myform.component';
+import { RouterModule } from '@angular/router';
+import { AllFormsComponent } from './all-forms/all-forms.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyformComponent,
+    AllFormsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'myform',
+        component: MyformComponent
+      }
+    ]),
+    RouterModule.forRoot([
+      {
+        path: 'all-forms',
+        component: AllFormsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
